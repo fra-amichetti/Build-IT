ab97437a1f7bc9629ffa2247520ba982d5f218b3
martiitesti-rgb <martiitesti@gmail.com>
2026-06-17 12:27:18 +0200

    modifica http

--- backend/src/main/java/com/buildit/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/buildit/backend/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://build-it-iota.vercel.app"));
+        config.setAllowedOrigins(List.of("https://buildit-gestionale.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
